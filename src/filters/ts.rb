90c001cd426d0802e77d09462208653c2bcfe62f
Sam Noedel <sam@samnoedel.com>
2014-11-20 19:26:51 -0800

    Removing unused files

--- src/filters/ts.rb
+++
@@ -1,7 +0,0 @@
-module Bowser
-  class Document
-    field :created, type: DateTime
-    field :modified, type: DateTime
-  end
-end
-
--- src/models/file.rb
+++
@@ -1,8 +0,0 @@
-require 'mongoid'
-
-module Bowser
-  class File
-    include Mongoid::Document
-    field :path, type: String
-  end
-end
