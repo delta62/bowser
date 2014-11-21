02b56b3fe4714758403116543ea2021d94eff95d
Sam Noedel <sam@samnoedel.com>
2014-11-21 12:41:35 -0800

    Fixing top level load path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'src'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'coveralls'
 Coveralls.wear!
-
-require 'app'
 
